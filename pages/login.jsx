6c719b13f5718a5d092702a889fdb169bbe00f2f
Mandar Prakash Kulkarni <mandar4567@gmail.com>
2022-09-10 00:03:34 +0530

    login updated

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const Login = () => {
   return (
   <>
-    <h1>Login</h1>
+  <div className='flex justify-center bg-slate-900 m-6 rounded-lg flex-col'>
+    <h1 className='text-white text-3xl m-5'>Login</h1>
+    <div className='flex-col text-white m-5 '>
+      <div className='m-2'>
+      <h3 className='text-white'>Username</h3>
+        <input className='rounded-sm' type="text" placeholder='Enter Username' />
+      </div>
+      <div className='m-2'>
+        <h3 className='text-white'>Password</h3>
+        <input className='rounded-sm' type="password" placeholder='Enter Password' />
+      </div>
+    </div>
+  </div>
+   
   </>
   )
 }
