e57495239879e01495fa5cad817565d644043c93
Prathamesh Karambelkar <83802999+prathameshkarambelkar@users.noreply.github.com>
2022-09-09 22:32:08 +0530

    Navbar change

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,24 +3,20 @@ import React from 'react'
 const Navbar = () => {
    return (
 
-      <header class="text-gray-400 bg-gray-900 body-font">
-         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-            <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-               <a class="mr-5 hover:text-white">First Link</a>
-               <a class="mr-5 hover:text-white">Second Link</a>
-               <a class="mr-5 hover:text-white">Third Link</a>
-               <a class="hover:text-white">Fourth Link</a>
+      <header class="text-gray-400 bg-gray-900 body-font ">
+         <div class="container justify-start items-left flex flex-wrap p-5 flex-col md:flex-row">
+            <nav class="flex lg:w-2/5 flex-wrap items-start justify-left text-base md:ml-auto ">
+               <a class="mr-5 hover:text-white">Home</a>
+               <a class="mr-5 hover:text-white">About</a>
             </nav>
             <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
-               <button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
-                  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                     <path d="M5 12h14M12 5l7 7-7 7"></path>
-                  </svg>
+               <button class="inline-flex items-center bg-gray-800 text-white border-0 py-1 px-3 mr-4 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Login
+               </button>
+               <button class="inline-flex items-center bg-gray-800 text-white border-0 py-1 px-3 mr-4 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Sign Up
                </button>
             </div>
          </div>
       </header>
-
 
    )
 }
