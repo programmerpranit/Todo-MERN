57394017fd52a590b21bdea5371facbd32382184
Pranit <dev.pranitpatil@gmail.com>
2022-09-09 22:39:40 +0530

    nav edits

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,12 +4,12 @@ const Navbar = () => {
    return (
 
       <header class="text-gray-400 bg-gray-900 body-font ">
-         <div class="container justify-start items-left flex flex-wrap p-5 flex-col md:flex-row">
-            <nav class="flex lg:w-2/5 flex-wrap items-start justify-left text-base md:ml-auto ">
+         <div class="container flex flex-wrap p-5 flex-col md:flex-row">
+            <nav class="flex lg:w-2/5 flex-wrap items-start justify-left text-base  ">
                <a class="mr-5 hover:text-white">Home</a>
                <a class="mr-5 hover:text-white">About</a>
             </nav>
-            <div class="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+            <div class="lg:w-3/5 inline-flex lg:justify-end ml-5 lg:ml-0">
                <button class="inline-flex items-center bg-gray-800 text-white border-0 py-1 px-3 mr-4 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Login
                </button>
                <button class="inline-flex items-center bg-gray-800 text-white border-0 py-1 px-3 mr-4 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Sign Up
